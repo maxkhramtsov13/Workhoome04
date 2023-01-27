d585857c2a9bb7c5059701619fa39f99d78e35ec
MaxKhramtsov <maxkhramtsov13@gmail.com>
2023-01-27 12:11:18 +0300

    домашка004

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Random rnd = new Random();
+long n = rnd.Next(10, 10000);
+System.Console.WriteLine(n);
+while ( n > 999 )
+n = n / 10;
+System.Console.WriteLine( n % 10 );
